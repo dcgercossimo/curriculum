c86ccc3d7b6a0da4f3a63037642974a28b3fffae
Daniel Gerçossimo <dcgercossimo@gmail.com>
2023-11-28 19:44:53 +0000

    Add scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Treinamento do https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 function Home() {
-    return (
-        <>
-            <h1>Rosângela meu amor!! Não sei o que seria da minha vida sem você! Quer casar comigo? Te amo!!!</h1>
-            <i>Daniel Gerçossimo</i>
-        </>
-    )
+  return (
+    <>
+      <h1>
+        Rosângela meu amor!! Não sei o que seria da minha vida sem você! Quer
+        casar comigo? Te amo!!!
+      </h1>
+      <i>Daniel Gerçossimo</i>
+    </>
+  );
+}
+
+function teste() {
+  console.log("identação errada");
 }
 
 export default Home;
