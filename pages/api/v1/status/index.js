9661d668d8cb579b0fdaab45fe5ca6c2f6f77a20
Daniel Gerçossimo <dcgercossimo@gmail.com>
2023-12-12 17:56:19 +0000

    add  fiiles

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query('SELECT 1 + 1 AS SUN;');
+  console.log(result.rows);
   res.status(200).json("endpoit status primeiro acesso só para os acima da média");
 }
 
