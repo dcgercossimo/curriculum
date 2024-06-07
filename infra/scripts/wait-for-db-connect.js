5c7c9a65464b5a3109dddca6c2aae3153e8974e5
DCGercossimo <dcgercossimo@gmail.com>
2024-06-07 20:35:22 -0300

    ajustes na visualização dos logs

--- infra/scripts/wait-for-db-connect.js
+++ infra/scripts/wait-for-db-connect.js
@@ -1,12 +1,18 @@
 const { exec } = require('node:child_process');
+const loadingSpinner = ["⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷"];
+let spinnerIndex = 0;
 
 function checkConnection() {
   exec('docker exec postgres-dev pg_isready --host localhost', handleReturn);
 
   function handleReturn(error, stdout) {
     if (stdout.search('accepting connections') === -1) {
-      process.stdout.write('.');
-      checkConnection();
+      process.stdout.clearLine();
+      process.stdout.cursorTo(0);
+      loading();
+      setTimeout(() => {
+        checkConnection()
+      }, 100);
       return;
     }
 
@@ -14,5 +20,10 @@ function checkConnection() {
   }
 }
 
-process.stdout.write("\n🔴 Aguardando banco aceitar conexões (¬_¬)");
+function loading() {
+  process.stdout.write(`🔴 Aguardando banco aceitar conexões (¬_¬) ${loadingSpinner[spinnerIndex++]} `);
+  spinnerIndex = spinnerIndex > 7 ? 0 : spinnerIndex;
+
+}
+
 checkConnection();
